86bd04893b1895e557ba8b6b1b6a39a6fa691a9a
Ali Taşcan <aliitascan@gmail.com>
2019-03-13 18:03:28 +0300

    index oluşturdum

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ali
+ * Date: 13.03.2019
+ * Time: 18:02
+ */ ?>
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>mindSoft</title>
+</head>
+<body>
+<h1>mindSoft</h1>
+</body>
+</html>
